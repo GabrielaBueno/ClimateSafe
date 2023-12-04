f4d712c3a0de03fcede9e2aad8ed8377c217c27d
Gabriela Bueno <68259978+GabrielaBueno@users.noreply.github.com>
2023-12-04 17:30:42 -0300

    Add files via upload

---
+++ pages/Previsao/mod/script.js
@@ -0,0 +1,54 @@
+let weather = {
+    apiKey: "dcf69f52c20b52683cc9c63634509b24",
+    fetchWeather: function (city) {
+      fetch(
+        "https://api.openweathermap.org/data/2.5/weather?q=" +
+          city +
+          "&units=metric&appid=" +
+          this.apiKey
+      )
+        .then((response) => {
+          if (!response.ok) {
+            alert("Não encontrado.");
+            throw new Error("Tente novamente.");
+          }
+          return response.json();
+        })
+        .then((data) => this.displayWeather(data));
+    },
+    displayWeather: function (data) {
+      const { name } = data;
+      const { icon, description } = data.weather[0];
+      const { temp, humidity } = data.main;
+      const { speed } = data.wind;
+      document.querySelector(".city").innerText = "" + name;
+      document.querySelector(".icon").src =
+        "https://openweathermap.org/img/wn/" + icon + ".png";
+      document.querySelector(".description").innerText = description;
+      document.querySelector(".temp").innerText = temp + "°C";
+      document.querySelector(".humidity").innerText =
+        "Umidade: " + humidity + "%";
+      document.querySelector(".wind").innerText =
+        "Vento: " + speed + " km/h";
+      document.querySelector(".weather").classList.remove("loading");
+      document.body.style.backgroundImage =
+        "url('https://images.pexels.com/photos/4894421/pexels-photo-4894421.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" + name + "')";
+    },
+    search: function () {
+      this.fetchWeather(document.querySelector(".search-bar").value);
+    },
+  };
+  
+  document.querySelector(".search button").addEventListener("click", function () {
+    weather.search();
+  });
+  
+  document
+    .querySelector(".search-bar")
+    .addEventListener("keyup", function (event) {
+      if (event.key == "Enter") {
+        weather.search();
+      }
+    });
+  
+  weather.fetchWeather("São Paulo");
---
+++ script.js
@@ -0,0 +1,18 @@
+const header = document.querySelector("head");
+
+window.addEventListener("scroll", function() {
+    header.classList.toggle("sticky", window.scrollY > 80);
+});
+
+function updateTime() {
+    const timeElement = document.getElementById('time');
+    const now = new Date();
+    const hours = now.getHours().toString().padStart(2, '0');
+    const minutes = now.getMinutes().toString().padStart(2, '0');
+    const seconds = now.getSeconds().toString().padStart(2, '0');
+    const timeString = `${hours}:${minutes}:${seconds}`;
+    timeElement.textContent = timeString;
+}
+
+setInterval(updateTime, 1000);
+updateTime();
