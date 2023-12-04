213a1df8396c697ccbc7431348553c036ece0181
Gabriela Bueno <68259978+GabrielaBueno@users.noreply.github.com>
2023-12-04 17:49:49 -0300

    Update script.js

--- pages/Previsao/mod/script.js
+++ pages/Previsao/mod/script.js
@@ -1,5 +1,28 @@
+const timeEl = document.getElementById('time');
+const dateEl = document.getElementById('date');
+const days = ['Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado', 'Domingo']
+const months = ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'];
+
+setInterval(() => {
+  const time = new Date();
+  const month = time.getMonth();
+  const date = time.getDate();
+  const day = time.getDay();
+  const hour = time.getHours();
+  const hoursIn12HrFormat = hour >= 13 ? hour %12: hour
+  const minutes = time.getMinutes();
+  const ampm = hour >=12 ? 'PM' : 'AM'
+
+  timeEl.innerHTML = (hoursIn12HrFormat < 10? '0'+hoursIn12HrFormat : hoursIn12HrFormat) + ':' + (minutes < 10? '0'+minutes: minutes)+ ' ' + `<span id="am-pm">${ampm}</span>`
+
+  dateEl.innerHTML = days[day] + ', ' + date+ ' ' + months[month]
+
+}, 1000);
+
+
 let weather = {
-    apiKey: "dcf69f52c20b52683cc9c63634509b24",
+    apiKey: "dcf69f52c20b52683cc9c63634509b24", 
+
     fetchWeather: function (city) {
       fetch(
         "https://api.openweathermap.org/data/2.5/weather?q=" +
